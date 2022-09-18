ae56fa8d71f9171228b3ec273542d6d99ebac4eb
Mehdi Khedhiri <xenm3hdi@hotmail.com>
2022-09-18 10:23:41 +0300

    first commit

---
+++ src/controllers/employee/createEmployee.js
@@ -0,0 +1,22 @@
+import Employee from "../../models/employee.model";
+import mongoose from "mongoose";
+import moment from "moment";
+
+export async function createEmployee(req, res) {
+    try {
+        const newEmployee = new Employee({
+            _id: new mongoose.Types.ObjectId(),
+            name : req.body.name,
+            firstName : req.body.firstName,
+            dateCreated : moment().format("YYYY-MM-DD"),
+            department : req.body.department
+        });
+
+        await newEmployee.save();
+
+        return res.status(200).json({success : true});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message : "unknown_error"})
+    }
+}
---
+++ src/controllers/employee/getListEmployees.js
@@ -0,0 +1,15 @@
+import Employee from "../../models/employee.model";
+import moment from "moment";
+
+export async function getListEmployees(req, res) {
+    try {
+        const query = req.body.creationDate ? { $eq: moment(req.body.creationDate).format("YYYY-MM-DD") } : { $exists: true }
+
+        const employees = await Employee.find({ dateCreated: query });
+
+        return res.status(200).json({ list: employees });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "unknown_error" })
+    }
+}
